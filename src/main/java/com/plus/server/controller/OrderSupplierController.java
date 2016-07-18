59bc4ed904644c968e2ff49c07fcd1c4513f41ba
chuly <chuly@chuly-PC>
2016-07-18 23:23:52 +0800

    111

--- src/main/java/com/plus/server/controller/OrderSupplierController.java
+++ src/main/java/com/plus/server/controller/OrderSupplierController.java
@@ -1,5 +1,7 @@
 package com.plus.server.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -8,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.github.pagehelper.PageInfo;
+import com.plus.server.common.vo.resp.BaseResp;
 import com.plus.server.model.OrderSupplier;
+import com.plus.server.model.OrderSupplierDetail;
 import com.plus.server.model.Organization;
 import com.plus.server.service.AssignTaskService;
 import com.plus.server.service.OrderSupplierService;
@@ -50,4 +55,51 @@ public class OrderSupplierController extends BaseController {
 	}
 
 
+	@RequestMapping(value = "/toEditDtl")
+	public ModelAndView toEditDtl(Model model, Long orderSupplierId) {
+		ModelAndView mv = new ModelAndView("orderSupplierDtl.ftl");
+		
+		model.addAttribute("orderSupplierId", orderSupplierId);
+		try {
+			OrderSupplier orderSupplier = orderSupplierService.selectById(orderSupplierId);
+			model.addAttribute("orderSupplier", orderSupplier);
+			List<OrderSupplierDetail> dtlList = orderSupplierService.selectDtlByPid(orderSupplierId);
+			model.addAttribute("dtlList", dtlList);
+		} catch (Exception e) {
+			log.error("", e);
+		}
+		return mv;
+	}
+	
+	@RequestMapping(value = "/updateDtl")
+	@ResponseBody
+	public BaseResp updateDtl(Long dtlId, String inDateStr, String outDateStr) {
+		BaseResp ret = new BaseResp();
+		if((inDateStr == null || inDateStr.isEmpty()) && (outDateStr == null || outDateStr.isEmpty())){
+			ret.setMsg("日期都为空");
+			return ret;
+		}
+		try {
+			SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+			Date inDate = null;//f.parse(inDateStr);
+			if(inDateStr != null && !"".equals(inDateStr)){
+				inDate = f.parse(inDateStr);
+			}
+			Date outDate = null;//f.parse(outDateStr);
+			if(outDateStr != null && !"".equals(outDateStr)){
+				outDate = f.parse(outDateStr);
+			}
+			OrderSupplierDetail dtl = new OrderSupplierDetail();
+			dtl.setId(dtlId);
+			dtl.setInStockTime(inDate);
+			dtl.setOutStockTime(outDate);
+			orderSupplierService.updateDtl(dtl);
+		} catch (Exception e) {
+			ret.setMsg(e.getMessage());
+			return ret;
+		}
+		ret.setSuccess(true);
+		return ret;
+	}
+	
 }
